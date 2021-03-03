f927665e68575e0fa15f5c3b1e8a8a8e05ec9163
Micah Banschick <micha.ban@gmail.com>
2021-03-03 23:51:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function fetchBooks() {
   fetch('https://anapioficeandfire.com/api/books')
   .then(resp => resp.json())
   .then(json => console.log(json));
-  renderBooks()
+  renderBooks(json)
 }
 
 function renderBooks(json) {
